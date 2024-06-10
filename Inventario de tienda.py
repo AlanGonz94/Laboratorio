501d12ed1c17cc29b71025201495397e8aadc1b1
AlanGonz94 <142268762+AlanGonz94@users.noreply.github.com>
2024-06-10 12:10:47 -0600

    Add files via upload

--- Inventario de tienda.py
+++ Inventario de tienda.py
@@ -19,8 +19,6 @@ Repetir el menú hasta que el usuario elija salir.
 """
 Inventario = []
 
-Producto = ["nombre","cantidad","precio"]
-
 print ("""
 Opcion 1 = agregar producto
 Opcion 2 = eliminar producto
@@ -30,12 +28,15 @@ Opcion 5 = salir
 
 """)
 Opcion = ""
+indice = -1
 
 while Opcion != 5:
     Opcion = input("Que opcion desea ejecutar ? 1 o 2 o 3 o 4 o 5    ")
 
     if Opcion == ("1"):  #Opcion 1 es igual a agregar producto y todas sus cosas
         
+        Producto = ["nombre","cantidad","precio"]
+
         Nombre = str(input("Ingrese el nombre de su producto  "))
         Producto[0] = Nombre
         Cantidad = str(input("Ingrese la cantidad de su producto  "))
@@ -44,7 +45,7 @@ while Opcion != 5:
         Producto[2] = ((", con precio de  Q") + (Precio))
 
         Inventario.append(Producto)
-        indice = 0
+        
         indice += 1
         for Producto in Inventario:
             for cosa in Producto:
@@ -54,34 +55,38 @@ while Opcion != 5:
 
     if Opcion == ("2"):  
         
-        Eliminar = ("Que producto eliminara? el primero, segundo, etc.  Responder 1 2 o 3, etc      ")
-        for Producto in Inventario: #eligiendo grupo por grupo en la lista de grupos
-            for x in range(0, 3): #metiendolos en los espacios 2,3,4 de la lista
-                Producto[Producto] = Inventario.remove([Eliminar]) #Aqui va cambiando el grupo cuando se llena y cada
-                
+        Eliminar = int(input("Que producto eliminara? el primero, segundo, etc.  Responder 0 1 2 o 3, etc      "))
+        Inventario.remove(Inventario[Eliminar]) #Aqui va cambiando el grupo cuando se llena y cada
+        
+        print ("El inventario ahora que se elimino eso se ve asi: ")
+        print("")
         for Producto in Inventario:
             for cosa in Producto:
                 print(cosa, end=" ")  #esto mostrara como se mira inventario actualizado
             print ()
 
+    if Opcion == ("3"):  
+        
+        Actualizar = int(input("Que producto actualizara? el primero, segundo, etc.  Responder 0 1 2 o 3, etc      "))
+        Cantidadnueva = str(input("Ingrese la cantidad de su producto  "))
+  
+        Inventario[Actualizar][1] = ((", Cantidad ") + (Cantidadnueva))
 
+        for Producto in Inventario:
+            for cosa in Producto:
+                print(cosa, end=" ")  #esto mostrara como se mira inventario actualizado
+            print ()
 
-"""
-#Crear una lista agarando los paises que no son cabezas de grupo
-paises_disponibles = [pais for pais in paises if pais not in [Cabeza1, Cabeza2, Cabeza3, Cabeza4, Cabeza5, Cabeza6, Cabeza7, Cabeza8]]
+    if Opcion == ("4"):  #consultar inventario
 
-# revolver las posiciones en la nueva lista
-random.shuffle(paises_disponibles)
-
-# Asignar aleatoriamente los demás países a los grupos
-indice = 0
-for grupo in Grupos: #eligiendo grupo por grupo en la lista de grupos
-    for x in range(2, 5): #metiendolos en los espacios 2,3,4 de la lista
-        grupo[x] = paises_disponibles[indice] #Aqui va cambiando el grupo cuando se llena y cada
-        indice += 1#pais de la lista de disponibles va avanzando al siguiente que haya recordando que tienen un nuevo orden
-
-for Grupo in Grupos:
-        for lugar in Grupo:
-            print(lugar, end=" ")  #esto mostrara como se mira grupos actualizado
-        print ()
-"""
+        print ("El inventario se ve asi: ")
+        print("")
+        for Producto in Inventario:
+            for cosa in Producto:
+                print(cosa, end=" ")  #esto mostrara como se mira inventario actualizado
+            print ()
+    
+    if Opcion == ("5"):  #consultar inventario
+        
+        Opcion == "5"
+        break
